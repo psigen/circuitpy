f374eeffd214e7fdb20694eb99416f79a7b7c25d
Pras Velagapudi <psigen@gmail.com>
2019-10-11 22:50:31 -0400

    Added everything but the tilemap.

--- dist/main.py
+++ dist/main.py
@@ -1,72 +1,69 @@
 import board
-import busio
-from digitalio import DigitalInOut
-import adafruit_esp32spi.adafruit_esp32spi_socket as socket
-from adafruit_esp32spi import adafruit_esp32spi
-import adafruit_requests as requests
+import displayio
+import adafruit_imageload
 
-# Get wifi details and more from a secrets.py file
-try:
-    from secrets import secrets
-except ImportError:
-    print("WiFi secrets are kept in secrets.py, please add them there!")
-    raise
+display = board.DISPLAY
 
-print("ESP32 SPI webclient test")
+# Load the sprite sheet (bitmap)
+sprite_sheet, palette = adafruit_imageload.load("/tiny_dungeon_world.bmp",
+                                                bitmap=displayio.Bitmap,
+                                                palette=displayio.Palette)
 
-TEXT_URL = "http://wifitest.adafruit.com/testwifi/index.html"
-JSON_URL = "http://api.coindesk.com/v1/bpi/currentprice/USD.json"
+# Create the sprite TileGrid
+sprite = displayio.TileGrid(sprite_sheet,
+                            pixel_shader=palette,
+                            width=1,
+                            height=1,
+                            tile_width=16,
+                            tile_height=16,
+                            default_tile=0)
 
-# If you are using a board with pre-defined ESP32 Pins:
-# esp32_cs = DigitalInOut(board.ESP_CS)
-# esp32_ready = DigitalInOut(board.ESP_BUSY)
-# esp32_reset = DigitalInOut(board.ESP_RESET)
+# Create the castle TileGrid
+castle = displayio.TileGrid(sprite_sheet,
+                            pixel_shader=palette,
+                            width=16,
+                            height=19,
+                            tile_width=16,
+                            tile_height=16)
 
-# If you have an externally connected ESP32:
-esp32_cs = DigitalInOut(board.D13)
-esp32_ready = DigitalInOut(board.D11)
-esp32_reset = DigitalInOut(board.D12)
+# Create a Group to hold the sprite and add it
+sprite_group = displayio.Group()
+sprite_group.append(sprite)
 
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+# Create a Group to hold the castle and add it
+castle_group = displayio.Group(scale=3)
+castle_group.append(castle)
 
-requests.set_socket(socket, esp)
+# Create a Group to hold the sprite and castle
+group = displayio.Group()
 
-if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
-    print("ESP32 found and in idle mode")
-print("Firmware vers.", esp.firmware_version)
-print("MAC addr:", [hex(i) for i in esp.MAC_address])
+# Add the sprite and castle to the group
+group.append(castle_group)
+group.append(sprite_group)
 
-for ap in esp.scan_networks():
-    print("\t%s\t\tRSSI: %d" % (str(ap['ssid'], 'utf-8'), ap['rssi']))
+# Castle tile assignments
+# corners
+castle[0, 0] = 3  # upper left
+castle[5, 0] = 5  # upper right
+castle[0, 4] = 9  # lower left
+castle[5, 4] = 11  # lower right
+# top / bottom walls
+for x in range(1, 5):
+    castle[x, 0] = 4  # top
+    castle[x, 4] = 10  # bottom
+# left/ right walls
+for y in range(1, 4):
+    castle[0, y] = 6  # left
+    castle[5, y] = 8  # right
+# floor
+for x in range(1, 5):
+    for y in range(1, 4):
+        castle[x, y] = 7  # floor
 
-print("Connecting to AP...")
-while not esp.is_connected:
-    try:
-        esp.connect_AP(secrets['ssid'], secrets['password'])
-    except RuntimeError as e:
-        print("could not connect to AP, retrying: ", e)
-        continue
-print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
-print("My IP address is", esp.pretty_ip(esp.ip_address))
-print("IP lookup adafruit.com: %s" %
-      esp.pretty_ip(esp.get_host_by_name("adafruit.com")))
-print("Ping google.com: %d ms" % esp.ping("google.com"))
+# put the sprite somewhere in the castle
+sprite.x = 110
+sprite.y = 70
 
-#esp._debug = True
-print("Fetching text from", TEXT_URL)
-r = requests.get(TEXT_URL)
-print('-' * 40)
-print(r.text)
-print('-' * 40)
-r.close()
-
-print()
-print("Fetching json from", JSON_URL)
-r = requests.get(JSON_URL)
-print('-' * 40)
-print(r.json())
-print('-' * 40)
-r.close()
-
-print("Done!")
+# Add the Group to the Display
+while True:
+    display.show(group)
---
+++ dist/wifi.py
@@ -0,0 +1,41 @@
+from adafruit_esp32spi import adafruit_esp32spi
+from digitalio import DigitalInOut
+import adafruit_esp32spi.adafruit_esp32spi_socket as socket
+import adafruit_requests as requests
+import board
+import busio
+
+# Get wifi details and more from a secrets.py file
+try:
+    from secrets import secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+# Externally connected ESP32:
+esp32_cs = DigitalInOut(board.D13)
+esp32_ready = DigitalInOut(board.D11)
+esp32_reset = DigitalInOut(board.D12)
+
+spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+
+requests.set_socket(socket, esp)
+
+if esp.status == adafruit_esp32spi.WL_IDLE_STATUS:
+    print("ESP32 found and in idle mode")
+print("Firmware vers.", esp.firmware_version)
+print("MAC addr:", [hex(i) for i in esp.MAC_address])
+
+print("Connecting to ", str(secrets['ssid']), "...")
+while not esp.is_connected:
+    try:
+        esp.connect_AP(secrets['ssid'], secrets['password'])
+    except RuntimeError as e:
+        print("could not connect to AP, retrying: ", e)
+        continue
+
+print("Connected to", str(esp.ssid, 'utf-8'), "\tRSSI:", esp.rssi)
+print("My IP address is", esp.pretty_ip(esp.ip_address))
+print("IP lookup adafruit.com: %s" %
+      esp.pretty_ip(esp.get_host_by_name("adafruit.com")))
